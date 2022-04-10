2f1d6f7e1ec9949ff36b5a97c3efbb7671358657
ecoknows <jerico.villaraza19@gmail.com>
2022-04-10 19:39:10 +0800

    Remove Credentials

--- models/recipe.model.js
+++ models/recipe.model.js
@@ -3,9 +3,15 @@ const mongoose =  require("mongoose");
 const recipeSchema = new mongoose.Schema(
 	{
 		name: {type: String, required: true},
-		description: {type: String, required: true},
+		description: { type: String, required: true},
 		reference: { type: String, required: true},
-		ingredients: { type: [{type: String}], required: true},
+
+		categories: [String],	
+
+		ingredients: [{ quantity: Number, name: String }],
+		
+		instructions: [String],
+
 		displayPhoto: { type: String, required: true},
 	},
 	{
